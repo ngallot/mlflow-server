e4d37622bc0db78d47a9ea85ee3694e0424adf04
ngallot <nicolas.gallot@gmail.com>
2020-03-03 21:51:09 +0000

    working config on cloud run

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.7-slim-stretch
 
 # Install dependencies
 ADD requirements.txt ./
-RUN python -m pip install -r requirements.txt
+RUN python -m pip install --upgrade pip -r requirements.txt
 
 ADD startup.sh ./startup.sh
 RUN chmod +x ./startup.sh
--- startup.sh
+++ startup.sh
@@ -1,4 +1,6 @@
 mlflow server \
     --host 0.0.0.0 \
-    --gunicorn-opts "--log-level DEBUG --timeout 90" \
+    --gunicorn-opts "--log-level DEBUG --timeout 300 --proxy-allow-from 0.0.0.0 --forwarded-allow-ips 0.0.0.0" \
+    --backend-store-uri gs://mlflow-public \
+    --default-artifact-root gs://mlflow-public \
     --port 5000
